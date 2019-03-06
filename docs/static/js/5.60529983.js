b026e2af44b28f8c8cd1eb3068edc32dfd2d8700
Jaume Tarradas Llort <jaume@bic.cat>
2019-03-06 12:08:01 +0100

    docz update + docs folder generated

---
+++ docs/static/js/5.60529983.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{"./.docz/app/imports.js":function(n,o,e){"use strict";e.r(o),e.d(o,"imports",function(){return s});var s={"src/components/AutoScaleSprite/docs/index.mdx":function(){return Promise.all([e.e(0),e.e(3)]).then(e.bind(null,"./src/components/AutoScaleSprite/docs/index.mdx"))},"src/components/AutoScaleSprite/docs/max-dimensions.mdx":function(){return Promise.all([e.e(0),e.e(4)]).then(e.bind(null,"./src/components/AutoScaleSprite/docs/max-dimensions.mdx"))}}}},0,[0,3,4]]);
+//# sourceMappingURL=5.72b73811cfa60e66623a.js.map
